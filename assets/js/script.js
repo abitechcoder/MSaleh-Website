3a51f059e24cb4a1cafe431fc841153d69aab702
Adeosun Abiola <abiolaolalekan39@gmail.com>
2022-04-05 16:47:52 +0100

    First Commit for this project

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+// Get the Scroll to top button
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+console.log(scrollToTopBtn);
+// Get the root element of the document
+var rootElement = document.documentElement;
+
+function handleScroll() {
+  // Do something on scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  console.log(scrollTotal);
+  console.log(rootElement.scrollTop / scrollTotal);
+  if (rootElement.scrollTop / scrollTotal > 0.7) {
+    // show button
+    scrollToTopBtn.style.display = "flex";
+  } else {
+    // Hide button
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+// Create a function called scrollToTop
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+// add a click event listener to the scroll to top button
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+document.addEventListener("scroll", handleScroll);
