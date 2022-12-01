0b4d60452d31e21a296c1fc4bb5c359ef086e845
abitechcoder <abiolaolalekan39@gmail.com>
2022-12-01 06:49:30 +0100

    Added Gallery Modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,14 @@ const contact_message = document.getElementById("message");
 const contact_form = document.getElementById("contactForm");
 const contact_alert = document.getElementById("contact_alert");
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get all small images and insert it inside the modal - use its "alt" text as a caption
+var smallImages = document.querySelectorAll(".smallImage");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
 /* Open when someone clicks on the span element */
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
@@ -46,6 +54,22 @@ function scrollToTop() {
 scrollToTopBtn.addEventListener("click", scrollToTop);
 
 document.addEventListener("scroll", handleScroll);
+
+smallImages.forEach((img) => {
+  img.addEventListener("click", () => {
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+  })
+})
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
 // const handleSubmit = async (e) => {
 //   e.preventDefault();
